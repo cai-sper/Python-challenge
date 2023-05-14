3bf3e9489b9c7d18843c62cbb9797ea08b588ab3
cai-sper <cailin.sperling@gmail.com>
2023-05-14 13:39:06 -0400

    Partial Code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,12 +5,37 @@
     # The percentage of votes each candidate won
     # The total number of votes each candidate won
     # The winner of the election based on popular vote
+# In addition, your final script should both print the analysis to the terminal and export a text file with the results.
 
+# Modules
 import os
 import csv
 
+# Set path for file
 election_path = os.path.join("Resources","election_data.csv")
 
+# Open CSV file and find the number of votes cast
 with open(election_path) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-    
+    next(csvreader)
+    data = list(csvreader)
+    votes = len(data)
+    print(f"Number of Votes Cast: {votes}")
+
+# A complete list of candidates who received votes
+candidates = {
+     "name": [
+            "Charles Casper Stockham",
+            "Diana DeGette",
+            "Raymon Anthony Doane"]}
+
+candidate_counts = [0, 0, 0]
+for i in range(0, int(votes)):
+    for j in range(3):
+        if data[i][2] == candidates["name"][j]:
+            candidate_counts[j] += 1
+for j in range(3):
+    vote_percent = round((candidate_counts[j]/votes)*100, 3)
+    print(f"{candidates['name'][j]}: {vote_percent}% ({candidate_counts[j]})")
+
+# The winner of the election based on popular vote
