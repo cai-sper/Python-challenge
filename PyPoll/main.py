401a2c1a1d13b3350cee81abba215d8c84b9d2be
cai-sper <cailin.sperling@gmail.com>
2023-05-14 14:01:32 -0400

    Final Analysis

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -30,7 +30,7 @@ candidates = {
             "Raymon Anthony Doane"]}
 
 candidate_counts = [0, 0, 0]
-for i in range(0, int(votes)):
+for i in range(int(votes)):
     for j in range(3):
         if data[i][2] == candidates["name"][j]:
             candidate_counts[j] += 1
@@ -39,3 +39,19 @@ for j in range(3):
     print(f"{candidates['name'][j]}: {vote_percent}% ({candidate_counts[j]})")
 
 # The winner of the election based on popular vote
+winner = (max(candidate_counts))
+winner_index = candidate_counts.index(winner)
+print(f"Winner: {candidates['name'][winner_index]}")
+
+# Print the analysis to the terminal (DONE STEP BY STEP) and export a text file with the results
+output_path = os.path.join("analysis", "analysis.txt")
+with open(output_path, 'w') as analysis_file:
+    analysis_file.write("Election Results\n")                                                     
+    analysis_file.write("--------------------------\n")
+    analysis_file.write(f"Total Votes: {votes}\n")
+    analysis_file.write("--------------------------\n")
+    analysis_file.write(f"{candidates['name'][0]}: {round((candidate_counts[0]/votes)*100, 3)}% ({candidate_counts[0]})\n")                                      
+    analysis_file.write(f"{candidates['name'][1]}: {round((candidate_counts[1]/votes)*100, 3)}% ({candidate_counts[1]})\n") 
+    analysis_file.write(f"{candidates['name'][2]}: {round((candidate_counts[2]/votes)*100, 3)}% ({candidate_counts[2]})\n")                         
+    analysis_file.write("--------------------------\n")    
+    analysis_file.write(f"Winner: {candidates['name'][winner_index]}\n")
